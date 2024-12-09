c329aa2256fdcc2b66898c49ec7143385406ae8d
yuta ono <yuta.ono@prum.jp>
2024-12-09 09:54:39 +0900

    3

--- JavaScript.js
+++ JavaScript.js
@@ -4,3 +4,30 @@ const age = 24;
 if(age === 24){
     console.log("t");
 };
+switch (100){
+    case 10:
+        console.log(10);
+        break;
+    case 100:
+        console.log(100);
+        break;
+    default:
+        console.log(error);
+        break;
+};
+
+let year = 2020;
+while (year < 2024){
+    year++;
+    console.log(year);
+};
+
+for(let i=1; i<=5; i++){
+    console.log(i);
+};
+
+let fluits=["apple", "banana"]
+console.log(fluits);
+console.log(fluits[1]);
+fluits[0]="りんご";
+console.log(fluits[0]);
