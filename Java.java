a5e59fe628c04381dbdad1951f352b393c0fbc1d
yuta-ono845 <yuta.ono@prum.jp>
2024-12-11 12:28:03 +0900

    Update Java.java
    java4

--- Java.java
+++ Java.java
@@ -1,13 +1,17 @@
 public class ClassName {
+    public static int number = 100;
+
     public static void main(String[] args) {
         
         //評価シート、バックエンド、No３のコード
-        public int number = 100;
         if(number == 100) {
             System.out.println("100");
         }
 
-        //評価シート、バックエンド、Noのコード
+        //評価シート、バックエンド、No4のコード
+        for(int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
 
         //評価シート、バックエンド、Noのコード
         //評価シート、バックエンド、Noのコード
